6d50bd911d36954f7609f6bfc472de40bed500d8
Kentarou Asai <asaknkn@gmail.com>
2021-07-22 16:27:20 +0900

    more jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+    <label htmlFor="bar">
+      bar
+    </label>
+    <input tyep="text" onChange={() => {console.log("I am")}}/>
+    </React.Fragment>
   );
 }
 
